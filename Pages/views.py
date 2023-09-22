c16f85a6cdc1b93fa5e348e1b8bd1b1ae0d5ab25
christianburke2022 <Christian.burke@snhu.edu>
2023-09-22 13:30:38 -0400

    Pages

--- Pages/views.py
+++ Pages/views.py
@@ -1,8 +1,9 @@
-
 from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
-# Create your views here.
+
+class AboutPageView(TemplateView):  # new
+    template_name = "about.html"
